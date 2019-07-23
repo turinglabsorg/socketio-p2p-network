d010883f3d678253aa5526c384b75d31bf344494
turinglabsorg <turinglabs@tuta.io>
2019-07-24 01:22:30 +0200

    added log file

--- peer.js
+++ peer.js
@@ -2,35 +2,41 @@ var app = require('express')();
 var server = require('http').Server(app);
 var inconn = require('socket.io')(server);
 var outconn = require('socket.io-client');
-var getPort = require('get-port')
+const fs = require('fs')
 
 var args = process.argv.slice(2);
+
+function log(toLog){
+    console.log(toLog)
+    var d = new Date().toLocaleString();
+    fs.appendFileSync('log', '[' + d + '] ' + toLog + '\n');
+  }
 
 async function init () {
     let port = args[0].replace('--port=','')
 
     server.listen(port);
-    console.log('Listening on ' + port)
+    log('Listening on ' + port)
 
     inconn.on('connection', function (socket) {
-        console.log('Peer connected.')
+        log('Peer connected.')
         setInterval(function(){
-            console.log('Sending message to peer.')
+            log('Sending message to peer.')
             socket.emit('message', 'ping');
         },3000)
         socket.on('response', function (data) {
-            console.log('Received response: ' + data);
+            log('Received response: ' + data);
         });
     });
 
     if(args[1] !== undefined){
         var socket = outconn.connect(args[1].replace('--peer=',''), {reconnect: true});
         socket.on('connect', function () {
-            console.log('Connected to peer.')
+            log('Connected to peer.')
         });
         socket.on('message', function (data) {
         socket.emit('response', 'pong')
-        console.log('Received message: ' + data)
+        log('Received message: ' + data)
         });
     }
 }
